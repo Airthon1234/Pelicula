7d194835ac64d54ec71cfc35fcd04c164f4ad41d
Airthon1234 <cipoteland@gmail.com>
2023-08-10 09:40:39 -0500

    pelicula completado

--- src/main/resources/static/js/web/pelicula/frmpelicula.js
+++ src/main/resources/static/js/web/pelicula/frmpelicula.js
@@ -85,8 +85,9 @@ function listarpeli(){
 				" data-idpelicula='" + value.idpelicula + "'" +
 				" data-titulopelicula='" + value.titulopelicula + "'" +
 				" data-duracion='" + value.duracion + "'" +
-				" data-iddirector='" + value.directores.nombredirector + "'" +
-				" data-idgenero='" + value.generos.nombregenero + "'" +
+				" data-sinopsis='" + value.sinopsis + "'" +
+				" data-iddirector='" + value.directores.iddirector + "'" +
+				" data-idgenero='" + value.generos.idgenero + "'" +
 				"><i class='fas fa-pen'></i></button>"+
 				"</td>" +
 				"<td>" +
@@ -100,3 +101,80 @@ function listarpeli(){
 		}
 	});
 }
+
+
+$(document).on("click", ".btnactualizarpelicula", function(){
+	$("#txtnompelicula").val($(this).attr("data-titulopelicula"));
+	$("#txtduracion").val($(this).attr("data-duracion"));
+	$("#txtsipnosis").val($(this).attr("data-sinopsis"));
+	$("#hddidregistropelicula").val($(this).attr("data-idpelicula"));
+	
+	$("#cbodirector").empty();
+	
+	var iddirector=$(this).attr("data-iddirector");
+	$.ajax({
+		type: "GET",
+		url: "/directores/listarDirectors",
+		dataType: "JSON",
+		success: function(resultado){
+			if(resultado.length > 0){
+				$.each(resultado, function(index, value){
+					$("#cbodirector").append(
+						`<option value="${value.iddirector}">
+							${value.nombredirector}</option>`
+					);
+				})
+				$("#cbodirector").val(iddirector);
+				}
+		}
+	});
+	
+	$("#cbogenero").empty();
+	
+	var idgenero=$(this).attr("data-idgenero");
+	$.ajax({
+		type: "GET",
+		url: "/genero/listarGeneros",
+		dataType: "JSON",
+		success: function(resultado){
+			if(resultado.length > 0){
+				$.each(resultado, function(index, value){
+					$("#cbogenero").append(
+						`<option value="${value.idgenero}">
+							${value.nombregenero}</option>`
+					);
+				})
+				$("#cbogenero").val(idgenero);
+				}
+		}
+	});
+	
+	
+	$("#modalpelicula").modal("show");
+});
+
+$(document).on("click", ".btneliminaridpelicula", function(){
+	$("#hddideliminarpelicula").val("");
+	$("#hddideliminarpelicula").val($(this).attr("data-idpelicula"));
+	$("#mensajeeliminar").text("¿Seguro de Eliminar el "+
+						$(this).attr("data-idpelicula")+"?");
+	
+	$("#modalEliminarPelicula").modal("show");
+});
+
+$(document).on("click", "#btneliminar", function(){
+	$.ajax({
+		type: "DELETE",
+		contentType: 'application/json',
+		url: "eliminarpelicula",
+		data: JSON.stringify({
+			idpelicula: $("#hddideliminarpelicula").val()
+		}),
+		success: function(resultado){
+			alert(resultado.mensaje);
+			listarpeli();
+		}
+	});
+	
+	$("#modalEliminarPelicula").modal("hide");
+})
